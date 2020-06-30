69bd00aebfdeef19810252ea8146e675dbc71f14
Ilyas Ziyaoglu <ilyasziyaoglu@gmail.com>
2020-06-30 20:05:53 +0300

    loading url added to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,5 @@ export const environment = {
   production: true,
   baseApiUrl: 'http://34.72.159.36:8000/',
   baseCDNUrl: 'http://34.72.159.36:5000/',
-  isAdmin: true,
+  loadingUrl: 'http://34.72.159.36:5000/z1pcpDCvLA5kf5SCqzPlkUxrkS3Cv5Ni.gif',
 };
